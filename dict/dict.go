0acc98eb2b7555d12d3a1c015dd79b728b57b78b
AlenaFedotova <nyonk@mail.ru>
2017-08-17 21:31:09 +0300

    Add code in dict.go

--- dict/dict.go
+++ dict/dict.go
@@ -1,12 +1,44 @@
 package dict
 
-import utf8
+import(
+	"os"
+	"log"
+	"bufio"
+	"math/rand"
+	"time"
+	"unicode/utf8"
+)
 
-var WordMap = map[string]bool{}
-var WordsOfAS = []string{}
+var WordMap map[string]bool
+var WordsOfAS []string
+var source rand.Source
+var randPtr *rand.Rand
 
-func Init(as int) {
-	return nil
+func Init(as int, path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	WordMap = make(map[string]bool)
+	
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		str := scanner.Text()
+		_, ok := WordMap[str]
+		if (utf8.RuneCountInString(str) == as && !ok) {
+			WordsOfAS = append(WordsOfAS, str)
+		}
+		WordMap[str] = true
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	
+	source = rand.NewSource(time.Now().UnixNano())
+	randPtr = rand.New(source)
 }
 
 func CheckWord(word string) bool {
@@ -15,7 +47,7 @@ func CheckWord(word string) bool {
 }
 
 func RandWordOfAS() string {
-	return ""
+	return WordsOfAS[randPtr.Intn(len(WordsOfAS))]
 }
 
 
