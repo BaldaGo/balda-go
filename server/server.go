c18c24f6a5592bb6880cebd980d12f515e393ba8
Boyarskikh Nikita <N02@yandex.ru>
2017-08-20 23:27:48 +0300

    Rewrite server to make it work async
    Now it's working with pool and prompt, login and listening users
    async

--- server/server.go
+++ server/server.go
@@ -9,12 +9,14 @@ package server
 
 import (
 	// System
-	"fmt"
+	"net"
 	"strconv"
+	"time"
 
 	// Third-party
 
 	// Project
+	"github.com/BaldaGo/balda-go/conf"
 	"github.com/BaldaGo/balda-go/logger"
 )
 
@@ -26,47 +28,87 @@ import (
  * which contributes sessions, users, games, scores and other
  */
 type Server struct {
-	host    string
-	port    uint
-	square  Square
-	handler telnet.Handler
+	host              string
+	port              uint
+	maxSessions       uint
+	readingBufferSize uint
+	WaitTime          time.Duration
+	Timeout           time.Duration
+	Pool              *Pool
+	MaxUsernameLength uint
+	Sessions          []Session
+	Users             map[uint]User
 }
-
-/// @class handler - handle telnet connection
-type handler struct{}
 
 /**
  * @brief Constructor of class Server
  * @return server Pointer to new Server object
+ *
+ * Make light and eazy fast initialisation of server directly
  */
-func NewServer() *Server {
-	return new(Server)
+func New(cfg conf.ServerConf) *Server {
+	s := new(Server)
+	s.host = cfg.Host
+	s.port = cfg.Port
+	s.maxSessions = cfg.MaxSessions
+	s.readingBufferSize = cfg.ReadingBufferSize
+	s.WaitTime = cfg.WaitTime
+	s.Timeout = cfg.Timeout
+
+	s.MaxUsernameLength = cfg.Game.MaxUsernameLength
+
+	logger.Log.Debug("New server created")
+	return s
 }
 
 /**
- * @brief Initialisation of Server
+ * @brief Initialisation of Game server
  *
- * Create handle and fill other fields of server
+ * Create area and dict, fill other heavy game fields of server
  */
-func (s *Server) PreRun() {
-	s.handler = handler{}
+func (s *Server) PreRun(cfg conf.ServerConf) {
+	//TODO: Init dictionary
+	s.Pool = NewPool(cfg.Concurrency)
+	s.Users = make(map[uint]User)
+	s.Sessions = make([]Session, cfg.NumberOfGames)
 
-	//TODO
+	for i := 0; i < len(s.Sessions); i++ {
+		s.Sessions[i].Game = NewGame()
+	}
+
+	s.Pool.Run()
+	logger.Log.Debugf("Server is configurated with next options: %+v\n", cfg)
 }
 
 /**
  * @brief Start Server on given host and port
- * @param[in] host Hostname where server will listen connections
- * @param[in] port Port where server will listen connections
- * @param[in] debug Flag, write debug info if given
  */
-func (s *Server) Run(host string, port uint, debug bool) error {
-	if err := telnet.ListenAndServe(host+":"+strconv.Itoa(int(port)), s.handler); err != nil {
-		logger.Log.Critical("Server failed to startup on: %s:%d (%s)", host, port, err.Error())
+func (s *Server) Run() error {
+	l, err := net.Listen("tcp", net.JoinHostPort(s.host, strconv.Itoa(int(s.port))))
+	if err != nil {
+		err = logger.Trace(err, "Can't establish tcp connection")
+		logger.Log.Critical(err.Error())
 		return err
 	}
+	defer l.Close()
 
-	//TODO
+	logger.Log.Infof("Server started listening on: %s:%d", s.host, s.port)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			err = logger.Trace(err, "Failed to accept request. Retrying...")
+			logger.Log.Critical(err.Error())
+			continue
+		}
+
+		logger.Log.Infof("New user connected from %s", conn.RemoteAddr())
+
+		if _, err := s.Pool.AddWithTimeout([]interface{}{s, conn}, s.Timeout*time.Millisecond); err != nil {
+			logger.Log.Critical("Can't accept new connection")
+		}
+	}
+
 	return nil
 }
 
@@ -76,33 +118,51 @@ func (s *Server) Run(host string, port uint, debug bool) error {
  * Unlock, free all allocated memory and handlers, save data
  */
 func (s *Server) PostRun() {
-	//TODO
+	s = nil
+	logger.Log.Debug("Server destroyed")
 }
 
 /**
  * @brief Goroutine, which called when new telnet connection established
- * @param[in] ctx Context of connection
- * @param[in] w io.Writer object
- * @param[in] r io.Reader object
+ * @param[in] c Connection with new listening client
+ * @param[in] pull Sessions pull (channel)
  *
- * Echo server
+ * Listening connection with new user
  */
-func (h handler) ServeTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
-	var buffer [1]byte // Seems like the length of the buffer needs to be small, otherwise will have to wait for buffer to fill up.
-	p := buffer[:]
+func (t *Task) work() interface{} {
+	var s *Server
+	var c net.Conn
+
+	s = t.args[0].(*Server)
+	c = t.args[1].(net.Conn)
+
+	user, err := s.login(c)
+	if err != nil {
+		logger.Tracef(err, "User from %s can't log in", c.RemoteAddr())
+		logger.Log.Warning(err.Error())
+		c.Close()
+		time.Sleep(s.WaitTime * time.Millisecond)
+		return err
+	}
+
+	c.Write([]byte("Hello," + user.login + "!\n"))
+
+	// Seems like the length of the buffer needs to be small, otherwise will have to wait for buffer to fill up
+	buffer := make([]byte, s.readingBufferSize)
 
 	for {
-		n, err := r.Read(p)
-
-		if n > 0 {
-			logger.Log.Info(fmt.Sprintf("Readed %d bytes from client", n))
-			//TODO: Validate and parse request, build and broad cast response
-			oi.LongWrite(w, p[:n]) // Echo
-		}
+		n, err := c.Read(buffer)
 
 		if err != nil {
-			logger.Log.Warning("An error occured: %s", err.Error())
+			logger.Log.Warningf("Communication error")
 			break
 		}
+
+		if n > 0 {
+			logger.Log.Debugf("Readed %d bytes from client", n)
+			//TODO: Validate and parse request, build and broad cast response
+		}
 	}
+
+	return nil
 }
